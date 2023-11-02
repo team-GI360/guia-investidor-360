c2ebb9b852e0e582c65ada52c9b34031d7e2b9e2
Andre Araujo <andre.araujosilva2006@gmail.com>
2023-11-02 00:28:22 -0300

    Criacao telas LCA LCI Poupança CDB

---
+++ script.js
@@ -0,0 +1,158 @@
+const AcaoFIIBody = `
+    <div class="div-square">
+        <h1>O que são Ações?</h1>
+        <p>
+            Ações são partes da propriedade de uma empresa que podem ser
+            compradas e vendidas no mercado financeiro. Os investidores que possuem ações
+            se tornam acionistas e têm a oportunidade de lucrar com o desempenho da
+            empresa por meio do aumento do valor das ações e do recebimento de dividendos.
+            As ações também podem ser voláteis e estão sujeitas a riscos de mercado.
+        </p>
+        <ul>
+            <li>Petrobras (PETR4) - R$ 32,62</li>
+            <li>Vale (VALE3) - R$ 65,87</li>
+            <li>Itaú Unibanco (ITUB4) - R$ 27,21</li>
+            <li>Bradesco (BBDC4) - R$ 14,38</li>
+            <li>Ambev (ABEV3) - R$ 13,21</li>
+        </ul>
+        <h1>O que são Fundos Imobiliários?</h1>
+        <p>
+            Fundos de Investimento Imobiliário (FIIs) são veículos de investimento em
+            ativos imobiliários, como imóveis comerciais. A diferença principal em relação às
+            ações é que FIIs não representam propriedade direta em empresas, mas sim em
+            imóveis, gerando renda de aluguéis e valorização das cotas. FIIs tendem a ser
+            menos voláteis do que ações e oferecem diversificação em imóveis.
+        </p>  
+        <ul>
+            <li>ARX DOVER RECEBÍVEIS FDO DE INV IMOB - Rentabilidade: 102.00%</li>
+            <li>CANUMA CAPITAL MULTIESTRATÉGIA FDO DE INV IMOB - Rentabilidade: 107.71%</li>
+            <li>INTER TEVA INDICE DE TIJOLO FUNDO DE INVESTIMENTO - Rentabilidade: 78.00%</li>
+            <li>AF INVEST CRI FDO. INV. IMOB - RECEBÍVEIS IMOB. - Rentabilidade: 96.05%</li>
+            <li>JASC RENDA VAREJO ESSENCIAL FDO DE INV IMOB - FII - Rentabilidade: 99.48%</li>
+        </ul>
+    </div>
+    `;
+
+const LCIBody = `
+    <div class="div-square">
+        <h1 class="italic-bold">LCI</h1>
+        <p>
+            <strong> A Letra de Crédito Imobiliário (LCI) </strong> é um tipo de investimento de renda fixa bastante popular no Brasil. Trata-se de um título emitido por instituições financeiras com o objetivo de captar recursos para financiar o setor imobiliário. Quando você investe em LCI, está, na verdade, emprestando dinheiro para o banco, que utiliza esses recursos para conceder empréstimos a pessoas que desejam comprar, construir ou reformar imóveis.
+        </p>
+
+        <p>
+            Uma característica fundamental da LCI é que ela é um investimento isento de imposto de renda para pessoas físicas residentes no Brasil. Além disso, oferece uma certa segurança ao investidor, pois é protegido pelo Fundo Garantidor de Créditos (FGC) em até R$ 250.000 por CPF e por instituição financeira.
+        </p>
+
+        <p>
+            A LCI é lastreada por financiamentos imobiliários garantidos por hipoteca ou alienação fiduciária de imóvel. Isso significa que os recursos investidos são respaldados por empréstimos a pessoas físicas e jurídicas no setor imobiliário. A quantidade emitida de LCIs e o prazo de vencimento não podem ultrapassar o tamanho do lastro e o vencimento dos direitos creditórios vinculados ao ativo.
+        </p>
+
+        <p>
+            Existem diferentes tipos de LCIs, com rendimentos prefixados, pós-fixados ou híbridos, dependendo das condições do mercado e do tipo de LCI em questão. Geralmente, quanto maior o prazo do investimento, maior é a taxa de rendimento oferecida pelo banco.
+        </p>
+
+        <p>
+            Uma das vantagens da LCI é que ela é mais indicada para objetivos de médio e longo prazo, pois geralmente possui um período de carência, durante o qual o investidor não pode resgatar o dinheiro investido, que normalmente é de 90 dias ou mais. Portanto, é importante ter um bom planejamento financeiro ao investir em LCI para garantir que você não precise do dinheiro antes do prazo estabelecido.
+        </p>
+
+        <p>
+            Em resumo, a Letra de Crédito Imobiliário é uma forma de investimento de renda fixa que oferece isenção de imposto de renda, é protegida pelo FGC e é lastreada por financiamentos imobiliários. É uma opção popular para quem busca segurança e um retorno estável sobre o investimento no mercado financeiro brasileiro.
+        </p>
+    </div>`;
+
+const LCABody = `
+    <div class="div-square">
+        <h1 class="italic-bold">LCA</h1>
+        <p>
+            <strong> A Letra de Crédito do Agronegócio (LCA) </strong> é um tipo de investimento de renda fixa emitido por instituições financeiras, tanto públicas quanto privadas, com o objetivo específico de captar recursos para financiar atividades no setor do agronegócio. Assim como a Letra de Crédito Imobiliário (LCI), a LCA é uma forma de captação de recursos utilizada pelos bancos, mas é exclusivamente direcionada ao setor agrícola, que é um pilar fundamental da economia brasileira.
+        </p>
+
+        <p>
+            stimento, os bancos remuneram os investidores que aplicam dinheiro na LCA e utilizam esses recursos para conceder empréstimos a empresas e produtores rurais no segmento do agronegócio. Os investidores, por sua vez, recebem uma remuneração pré-estabelecida, que pode variar de acordo com o valor investido, o prazo de vencimento e a taxa de juros acordada com a instituição financeira.
+        </p>
+
+        <p>
+            Uma característica importante da LCA é que ela é isenta de Imposto de Renda (IR) e também está protegida pelo Fundo Garantidor de Créditos (FGC). O FGC garante que, mesmo em caso de quebra da instituição financeira emissora, o investidor será ressarcido em até R$ 250 mil por instituição, com um teto de R$ 1 milhão por CPF.
+        </p>
+
+        <p>
+            A LCA é vinculada a direitos creditórios originários de negócios realizados entre produtores rurais (ou suas cooperativas) e terceiros, incluindo financiamentos ou empréstimos. Estes direitos creditórios, como a Cédula de Produto Rural (CPR), Certificado de Depósito Agropecuário (CDA), Cédula Rural Hipotecária (CRH), Warrant Agropecuário (WA) e Notas Promissórias Rurais, são utilizados como lastro para a emissão da LCA pelas instituições financeiras. Os valores captados por meio da LCA são, principalmente, destinados a empréstimos para produtores rurais, auxiliando indiretamente no desenvolvimento do agronegócio no país.
+        </p>
+
+        <p>
+            Em resumo, a LCA é uma opção de investimento de renda fixa com baixo risco, isenção de IR, garantia pelo FGC e vinculada ao financiamento do agronegócio. É uma escolha popular para investidores com perfil conservador que buscam uma opção de médio a longo prazo para diversificar sua carteira de investimentos.
+        </p>
+    </div>`;
+
+const CDBBody = `
+    <div class="div-square">
+        <h1 class="italic-bold">CDB</h1>
+        <p>
+            O Certificado de Depósito Bancário (CDB) é um instrumento de investimento de renda fixa emitido por instituições bancárias. Funciona como um empréstimo do investidor para o banco emissor, permitindo que o banco capte recursos para financiar suas atividades, tais como projetos, crescimento e pagamento de dívidas. Qualquer investidor pode adquirir um CDB, e este investimento é protegido pelo Fundo Garantidor de Créditos (FGC) até o limite de R$250 mil por CPF ou CNPJ em cada instituição financeira.
+        </p>
+
+        <p>
+            Existem três tipos principais de CDB: prefixado, pós-fixado e híbrido. No CDB prefixado, o investidor sabe exatamente a remuneração em reais que obterá até o vencimento, pois a taxa de juros é definida no momento da aplicação. No CDB pós-fixado, a rentabilidade está atrelada a um indicador, geralmente o CDI (Certificado de Depósito Interbancário), e pode variar ao longo do tempo. O CDB híbrido combina características dos dois tipos anteriores.
+        </p>
+
+        <p>
+            Ao adquirir um CDB, o investidor empresta dinheiro ao banco por um determinado prazo. Em troca, o banco paga juros sobre o valor investido. Ao final do prazo estabelecido, o investidor recebe de volta o valor investido mais os juros acordados. O valor do CDI, que geralmente serve como referência para a remuneração do CDB pós-fixado, segue de perto a Taxa Selic, que é a taxa básica de juros da economia.
+        </p>
+
+        <p>
+            O investimento mínimo para aplicação em CDB pode variar conforme o título e o emissor, podendo começar a partir de valores baixos, como R$1,00, em alguns casos. Entretanto, títulos que oferecem maiores taxas de retorno costumam exigir investimentos mínimos mais altos, podendo chegar a alguns milhares de reais. É importante que o investidor verifique as condições específicas de cada CDB, incluindo o prazo de vencimento, a taxa de remuneração e o investimento mínimo exigido, ao tomar decisões de investimento.
+        </p>
+
+    </div>`;
+
+const PoupancaBody = `
+    <div class="div-square">
+        <h1 class="italic-bold">Poupança</h1>
+        <p>
+            A poupança é um tipo de conta bancária regulada pelo Banco Central, criada especificamente para poupar dinheiro. Ela funciona como uma caderneta de poupança onde os consumidores podem depositar dinheiro e receber rendimentos mensais. A conta poupança possui algumas regras básicas, incluindo a limitação de movimentações mensais (2 saques, 2 transferências e 2 extratos) e não permite receber salários.
+        </p>
+
+        <p>
+            A taxa de rendimento da poupança é determinada pelo Banco Central e, em geral, é de 0,5% ao mês (6,17% ao ano), mais a variação da TR (taxa referencial). No entanto, quando a taxa básica de juros (Selic) fica igual ou menor a 8,5% ao ano, a poupança rende 70% da meta da taxa Selic ao ano, mensalizada, somada à TR.
+        </p>
+
+        <p>
+            A poupança é uma opção de investimento acessível e simples, aberta até mesmo para menores de idade, desde que representados por pais ou responsáveis legais. Ao contrário de outras formas de investimento, a poupança é isenta de taxas e tarifas, e os rendimentos não são tributados pelo Imposto de Renda.
+        </p>
+
+        <p>
+            Apesar de oferecer liquidez diária, os rendimentos da poupança são creditados mensalmente no dia de "aniversário" da conta, ou seja, no dia em que o depósito foi feito. Se o dinheiro for retirado antes desse dia, o investidor não receberá os rendimentos daquele mês. É importante mencionar que, em caso de falência do banco, o governo reembolsará os valores depositados na poupança até o limite de R$60 mil por pessoa.
+        </p>
+
+        <p>
+            Em resumo, a poupança é uma opção popular para quem deseja poupar dinheiro com baixo risco e facilidade de acesso.
+        </p>
+
+    </div>`;
+
+var buttonAcaoFII = document.getElementById("button-acao-fii");
+var buttonLCI = document.getElementById("button-lci");
+var buttonLCA = document.getElementById("button-lca");
+var buttonCDB = document.getElementById("button-cdb");
+var buttonPoupanca = document.getElementById("button-poup");
+
+buttonAcaoFII.addEventListener("click", function() {
+    document.getElementById("div-body").innerHTML = AcaoFIIBody;
+});
+
+buttonLCI.addEventListener("click", function() {
+    document.getElementById("div-body").innerHTML = LCIBody;
+});
+
+buttonLCA.addEventListener("click", function() {
+    document.getElementById("div-body").innerHTML = LCABody;
+});
+
+buttonCDB.addEventListener("click", function() {
+    document.getElementById("div-body").innerHTML = CDBBody;
+});
+
+buttonPoupanca.addEventListener("click", function() {
+    document.getElementById("div-body").innerHTML = PoupancaBody;
+});
